8bf5fc46c35192cf3b25c498e96aca6498b8d6ac
palla sudarshan <palla sudarshan@DESKTOP-5ICMDD7>
2022-03-28 14:47:49 +0530

    first commit

---
+++ com.yantar/src/test/java/com/yantar_org/CretaeorgtTest.java
@@ -0,0 +1,12 @@
+package com.yantar_org;
+
+import org.testng.annotations.Test;
+
+public class CretaeorgtTest {
+	
+	@Test
+	public void createI() {
+		System.out.println("hi sud");
+	}
+
+}
---
+++ com.yantar/src/test/java/com/yantar_org/creaTest.java
@@ -0,0 +1,11 @@
+package com.yantar_org;
+
+import org.testng.annotations.Test;
+
+public class creaTest {
+	@Test
+	public void createdI() {
+		System.out.println("hi sudarshan");
+	}
+
+}
