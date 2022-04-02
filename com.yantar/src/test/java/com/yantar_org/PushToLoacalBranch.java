00ceaa9de46009c2b78dbec62b8e09a25c011c6f
aman-Automation2276 <aman2276@gmail.com>
2022-04-02 10:41:31 +0530

    push to local branch

---
+++ com.yantar/src/test/java/com/yantar_org/PushToLoacalBranch.java
@@ -0,0 +1,12 @@
+package com.yantar_org;
+
+import org.testng.annotations.Test;
+
+public class PushToLoacalBranch {
+
+	@Test
+	public void pushToLocalBranch()
+	{
+		System.out.println("code push to local branch");
+	}
+}
