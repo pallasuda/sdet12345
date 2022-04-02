a232c55628d99f1ce8d19df362e073ded05b56e4
aman-Automation2276 <aman2276@gmail.com>
2022-04-02 11:53:45 +0530

    push 2nd update to local branch

---
+++ com.yantar/src/test/java/com/yantar_org/PushToLocalBranchTest.java
@@ -0,0 +1,12 @@
+package com.yantar_org;
+
+import org.testng.annotations.Test;
+
+public class PushToLocalBranchTest {
+
+	@Test
+	public void push()
+	{
+		System.out.println("Push the code local branch");
+	}
+}
